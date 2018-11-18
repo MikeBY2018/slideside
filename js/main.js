cbbfa56c81b3f8a372d9f7932ad46b9cd428babc
MikeBY2018 <babskij2015@gmail.com>
2018-11-18 04:22:43 +0300

    Scroll pages

--- js/main.js
+++ js/main.js
@@ -34,3 +34,55 @@ var page = function(){
 }
 	console.log(page());
 //----------------------------------------
+
+/*var pagesScroll = [];
+	pagesScroll[0] = '../index.html';
+	pagesScroll[1] = '../work/page_02.html';
+	pagesScroll[2] = '../work/page_03.html';
+	pagesScroll[3] = '../work/page_04.html';*/
+
+
+/*window.onload = function (e){
+	console.log(e);
+}*/
+
+var pagesScroll = document.querySelectorAll('div.pages a');
+var j = 0;
+var a = 0;
+
+
+
+for(i = 0; i < pagesScroll.length; i++){
+	if(pagesScroll[i].hasAttribute('data-number-pages')){
+		j = i - 1;
+		a = i + 1;
+		if(j < 0){
+			j = pagesScroll.length - 1;
+		}
+		if(a >= pagesScroll.length){
+			a = 0;
+		}
+	}
+}
+document.onwheel = function(event){
+	if(event.deltaY < 0){
+	window.location.href = pagesScroll[j];
+	} else {
+	window.location.href = pagesScroll[a];	
+	}
+
+}
+
+
+/*
+
+
+	var a = '';
+	for(i = 0; i < pagesScroll.length; i++){
+		a += pagesScroll[i];
+	}
+	console.log(pagesScroll);
+	if(event.deltaY < 0){
+		/*window.location.href = pagesScroll[0];*/
+	/*};
+}*/
