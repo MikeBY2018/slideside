cb20794b98b9ad36c9658989b47219dd40c7d055
MikeBY2018 <babskij2015@gmail.com>
2018-12-10 21:26:48 +0300

    fixed starsbackground

--- js/main.js
+++ js/main.js
@@ -4,13 +4,22 @@ window.onload = function(){
 	var portfolio = document.getElementById('portfolio');
 	var portfolioCo = document.getElementById('modal-overlay');
 	var backgSlide = document.getElementsByClassName('background');
-	var bodyClass = doc
 
-	var navMenu = document.querySelectorAll('.cl-effect-4 li a');
 
-	for(var i = 0; i < navMenu.length; i+=2){
-		navMenu[i].onclick = thisPage;
+	var clickMenu = document.querySelectorAll('.cl-effect-4 li a');
+	var pagination = document.querySelectorAll('.onepage-pagination a');
+	
+	
+	clickMenu[0].onclick = function(){
+		pagination[1].onclick = function(){};
 	}
+
+	
+	// var navMenu = document.querySelectorAll('.cl-effect-4 li a');
+
+	// for(var i = 0; i < navMenu.length; i+=2){
+	// 	navMenu[i].onclick = thisPage;
+	// }
 }
 
 function thisPage() {
--- js/scroll.js
+++ js/scroll.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	
 	$(".main").onepage_scroll({
 	   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-	   easing: "ease-in-out",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+	   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
 	                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
 	   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
 	   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
 	   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-	   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-	   afterMove: function(){}, // This option accepts a callback function. The function will be called after the page moves.
+	   beforeMove: function(index) {animateStars();},  // This option accepts a callback function. The function will be called before the page moves.
+	   afterMove: function(index){}, 		// This option accepts a callback function. The function will be called after the page moves.
 	   loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
 	   keyboard: true,                  // You can activate the keyboard controls
 	   responsiveFallback: 550,        // You can fallback to normal page scroll by defining the width of the browser in which
@@ -16,13 +16,43 @@ $(document).ready(function(){
 	                                    // the browser's width is less than 600, the fallback will kick in.
 	   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
 	});
+
 });	
 
+function animateStars(){
+		var classNameBody = document.body.className;
+		var pageClass = $(".cl-effect-4 li a");
+		
+
+		var starCanvasHeight;
+
+		if(classNameBody == 'viewing-page-1'){
+			starCanvasHeight = 0;
+			remThisPageClass();
+		}else if(classNameBody == 'viewing-page-2'){
+			starCanvasHeight = 100;
+			remThisPageClass();
+			pageClass[0].classList.add('this_page');
+			
+		}else if(classNameBody == 'viewing-page-3'){
+			starCanvasHeight = 200;
+			remThisPageClass();
+		}else if(classNameBody == 'viewing-page-4'){
+			starCanvasHeight = 300;
+			remThisPageClass();
+			pageClass[2].classList.add('this_page');
+		}
+		canvas1.css({ 
+			'transform': 'translate(0 ,' + starCanvasHeight + '%)',
+			'-webkit-transform': 'translate(0, ' + starCanvasHeight + '%)',
+			'-ms-transform': 'translate(0, ' + starCanvasHeight + '%)',
+	    }); 
+
+	function remThisPageClass(){
+		for(i = 0; i < 3; i++){
+			pageClass[i].classList.remove('this_page');
+		}
+	}   
+}
 
 
-// csHeight = 100;
-// 				canvas1.css({ 
-//            			'transform': 'translate(0 ,' + csHeight + '%)',
-//            			'-webkit-transform': 'translate(0, ' + csHeight + '%)',
-//             		'-ms-transform': 'translate(0, ' + csHeight + '%)',
-//         	    }); 
