2834ff08689686f98ecef3f0e77d579e3028e64f
MikeBY2018 <babskij2015@gmail.com>
2018-12-26 23:31:23 +0300

    add form at click in page02

--- js/scroll.js
+++ js/scroll.js
@@ -8,6 +8,7 @@ $('document').ready(function() {
   	$('#payment').load('../work/template/payment.html');
   	$('#other').load('../work/template/other.html');
   	$('#welcomepage').load('../work/template/form.html');
+  	$('#welcomepage1').load('../work/template/form.html');
   	$('#formfinish').load('../work/template/form.html');
 
 	$(function(){
