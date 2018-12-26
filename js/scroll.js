2241cac8b852b3d7b358c01d38c8a0f8bb01a765
MikeBY2018 <babskij2015@gmail.com>
2018-12-26 23:05:50 +0300

    unk

--- js/scroll.js
+++ js/scroll.js
@@ -66,7 +66,7 @@ $(".main").onepage_scroll({
    animationTime: 600,             // AnimationTime let you define how long each section takes to animate
    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-   beforeMove: function(index) {animateStars();},  // This option accepts a callback function. The function will be called before the page moves.
+   beforeMove: function(index) {/*animateStars();*/},  // This option accepts a callback function. The function will be called before the page moves.
    afterMove: function(index){}, 		// This option accepts a callback function. The function will be called after the page moves.
    loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
    keyboard: true,                  // You can activate the keyboard controls
@@ -76,45 +76,45 @@ $(".main").onepage_scroll({
    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
 });
 
-function animateStars(){
-		var classNameBody = document.body.className;
-		var pageClass = $(".cl-effect-4 li a");
-		canvas = $('#space');
+// function animateStars(){
+// 		var classNameBody = document.body.className;
+// 		var pageClass = $(".cl-effect-4 li a");
+// 		canvas = $('#space');
 
 
-		var starCanvasHeight;
+// 		var starCanvasHeight;
 
-		if(classNameBody == 'viewing-page-1'){
-			starCanvasHeight = 0;
-			remThisPageClass();
-		}else if(classNameBody == 'viewing-page-2'){
-			starCanvasHeight = 100;
-			remThisPageClass();
-			pageClass[0].classList.add('this_page');
+// 		if(classNameBody == 'viewing-page-1'){
+// 			starCanvasHeight = 0;
+// 			remThisPageClass();
+// 		}else if(classNameBody == 'viewing-page-2'){
+// 			starCanvasHeight = 100;
+// 			remThisPageClass();
+// 			pageClass[0].classList.add('this_page');
 			
-		}else if(classNameBody == 'viewing-page-3'){
-			starCanvasHeight = 200;
-			remThisPageClass();
-		}else if(classNameBody == 'viewing-page-4'){
-			starCanvasHeight = 300;
-			remThisPageClass();
-			pageClass[2].classList.add('this_page');
-		}else if(classNameBody == 'viewing-page-5'){
-			starCanvasHeight = 400;
-			remThisPageClass();
-		}
-		canvas.css({ 
-			'transform': 'translate(0 ,' + starCanvasHeight + '%)',
-			'-webkit-transform': 'translate(0, ' + starCanvasHeight + '%)',
-			'-ms-transform': 'translate(0, ' + starCanvasHeight + '%)',
-	    }); 
+// 		}else if(classNameBody == 'viewing-page-3'){
+// 			starCanvasHeight = 200;
+// 			remThisPageClass();
+// 		}else if(classNameBody == 'viewing-page-4'){
+// 			starCanvasHeight = 300;
+// 			remThisPageClass();
+// 			pageClass[2].classList.add('this_page');
+// 		}else if(classNameBody == 'viewing-page-5'){
+// 			starCanvasHeight = 400;
+// 			remThisPageClass();
+// 		}
+// 		canvas.css({ 
+// 			'transform': 'translate(0 ,' + starCanvasHeight + '%)',
+// 			'-webkit-transform': 'translate(0, ' + starCanvasHeight + '%)',
+// 			'-ms-transform': 'translate(0, ' + starCanvasHeight + '%)',
+// 	    }); 
 
-		function remThisPageClass(){
-			for(i = 0; i < 3; i++){
-				pageClass[i].classList.remove('this_page');
-			}
-		};   
-}
+// 		function remThisPageClass(){
+// 			for(i = 0; i < 3; i++){
+// 				pageClass[i].classList.remove('this_page');
+// 			}
+// 		};   
+// }
 
 $('#course').on('click', function(){$(".main").moveTo(2);});
 $('#contacts').on('click', function(){$(".main").moveTo(4);});
