8c1e4fc3c9454ef3b5ba3e5eee5819797094723f
MikeBY2018 <babskij2015@gmail.com>
2019-01-10 01:44:13 +0300

    Настроена отправка почты

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
 <?php 
 if (!empty($_POST['name']) AND !empty($_POST['phone'])){
-    $headers = 'From: babskij2015@gmail.com' .
+    $headers = 'From: <babskij2015@gmail.com>' .
                 'Reply-To: groupby@ormedia.by\r\n' .
                 'X-Mailer: PHP/' . phpversion();
          
-    $theme = "Новая заявка с сайта rvgroup.by";             
+    $theme = "Новая заявка с сайта ormedia.by";             
              
     $letter = "Данные сообщения:";
     $letter .="\n\n";
@@ -13,7 +13,7 @@ if (!empty($_POST['name']) AND !empty($_POST['phone'])){
     $letter .="\nКурс: ".$_POST['front'];
     $letter .="\nО себе: ".$_POST['message'];
     
-    if (mail("babskij2015@gmail.com", $theme, $letter, $headers)){
+    if (mail("<babskij@yandex.com>", $theme, $letter, $headers)){
         echo "Сообщение отправлено!";
   } else {
         echo "Ошибка при отправке!";
