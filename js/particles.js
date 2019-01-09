473cd0e690254ed46e397045de79d3c1a076b84c
AlexeyPoll <alexeypol7@gmail.com>
2019-01-09 19:52:57 +0300

    particles

--- js/particles.js
+++ js/particles.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-  particlesJS.load('particles-js', 'assets/particles.json', function() {
+  particlesJS.load('particles1-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  particlesJS.load('particles2-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  particlesJS.load('particles3-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  particlesJS.load('particles4-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 })
-
